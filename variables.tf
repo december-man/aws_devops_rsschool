7c2570a2e1009d80a6e7190feb854b6d9907307f
december-man <aspasjutin@outlook.com>
2024-10-11 22:07:00 +0300

    add necessary variables for subnet, vpc & ec2 setup

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# Task 1: AWS Account variables
+
 variable "region" {
   description = "AWS Region"
   type        = string
@@ -22,4 +24,49 @@ variable "iam_policies" {
     "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
     "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess" # For future dynamodb setup of S3 backend
   ]
+}
+
+# Task 2: networking variables
+
+variable "vpc_cidr" {
+  description = "CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["eu-central-1a", "eu-central-1b"]
+}
+
+# Task 2: EC2 variables
+
+variable "ec2_amazon_linux_ami" {
+  description = "EC2 Instance Image for Bastion Host and Testing"
+  default = "ami-0592c673f0b1e7665"
+}
+
+variable "ssh_pubkey_file" {
+  description = "Path to an SSH public key"
+  default     = "~/.ssh/aws/key.pub"
 }
