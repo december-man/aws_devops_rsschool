e086a5bb8e52747b1e7574c6da52fa3b4164ba5f
december-man <aspasjutin@outlook.com>
2024-10-12 14:58:38 +0300

    eip.tf fix: terraform fails to build with domain attribute, switch back

--- eip.tf
+++ eip.tf
@@ -1,7 +1,7 @@
 # Task 2: Networking Resources
 
 resource "aws_eip" "public_ip" {
-  domain            = "vpc"
+  vpc               = true
   network_interface = aws_network_interface.nat_interface.id
   tags = {
     Name = "Public IP for NAT Instance"
