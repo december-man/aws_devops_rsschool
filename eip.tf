e0745aaad850ba189ae3a3b624f22f1451615c36
december-man <aspasjutin@outlook.com>
2024-10-12 14:56:05 +0300

    eip.tf fix: rm deprecated vpc attribute

--- eip.tf
+++ eip.tf
@@ -1,7 +1,7 @@
 # Task 2: Networking Resources
 
 resource "aws_eip" "public_ip" {
-  vpc               = true
+  domain            = "vpc"
   network_interface = aws_network_interface.nat_interface.id
   tags = {
     Name = "Public IP for NAT Instance"
