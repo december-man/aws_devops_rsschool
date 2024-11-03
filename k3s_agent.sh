e28c9814fcf9fb19d5840fb304bc3381302b7148
december-man <aspasjutin@outlook.com>
2024-11-03 14:58:11 +0300

    Helm installation

--- k3s_agent.sh
+++ k3s_agent.sh
@@ -5,5 +5,3 @@ sudo yum install selinux-policy-targeted -y
 
 # Install k3s Agent
 curl -sfL https://get.k3s.io | K3S_URL="https://${server_addr}:6443" K3S_TOKEN=${token} sh -s -
-
-# Install Telnet
--- k3s_server.sh
+++ k3s_server.sh
@@ -11,10 +11,15 @@ curl -sfL https://get.k3s.io | K3S_TOKEN=${token} sh -s -
 sudo ln -s /usr/local/bin/k3s /usr/bin/k3s
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown ec2-user:ec2-user ~/.kube/config
 sudo chmod 600 ~/.kube/config
 echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
 source ~/.bashrc
 
 # Install Telnet
+sudo yum install telnet -y
 
 # Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
